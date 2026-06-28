8f6be79df712bdcb1b3790143609c1386aec8736
lokesh1982-alt <goset.lokesh@gmail.com>
2026-06-28 16:43:57 +0100

    Add files via upload

--- as-sw.js
+++ as-sw.js
@@ -1,4 +1,11 @@
-const CACHE='artha-sangraha-v1';
+const CACHE='artha-sangraha-v3';
 const ASSETS=['./artha-sangraha.html','./as-manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
